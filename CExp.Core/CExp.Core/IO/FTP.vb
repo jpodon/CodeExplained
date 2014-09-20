dc9ff1fc27d75d5ef80bef5d6bd599cbb27d1f01
jpodon <jpodon@hotmail.co.uk>
2014-09-20 16:39:32 +0100

    ListFTPFiles

---
+++ CExp.Core/CExp.Core/IO/FTP.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Net
+Imports System.IO
+
+Namespace IO
+
+    Public Class FTP
+        Public Shared Function ListFTPFiles(ftpAddress As String, ftpUser As String, ftpPassword As String, ExceptionInfo As Exception) As List(Of String)
+
+            Dim ListOfFilesOnFTPSite As New List(Of String)
+
+            Dim ftpRequest As FtpWebRequest = Nothing
+            Dim ftpResponse As FtpWebResponse = Nothing
+
+            Dim strReader As StreamReader = Nothing
+            Dim sline As String = ""
+
+            Try
+                ftpRequest = CType(WebRequest.Create(ftpAddress), FtpWebRequest)
+
+                With ftpRequest
+                    .Credentials = New NetworkCredential(ftpUser, ftpPassword)
+                    .Method = WebRequestMethods.Ftp.ListDirectory
+                End With
+
+                ftpResponse = CType(ftpRequest.GetResponse, FtpWebResponse)
+
+                strReader = New StreamReader(ftpResponse.GetResponseStream)
+
+                If strReader IsNot Nothing Then sline = strReader.ReadLine
+
+                While sline IsNot Nothing
+                    ListOfFilesOnFTPSite.Add(sline)
+                    sline = strReader.ReadLine
+                End While
+
+            Catch ex As Exception
+                ExceptionInfo = ex
+
+            Finally
+                If ftpResponse IsNot Nothing Then
+                    ftpResponse.Close()
+                    ftpResponse = Nothing
+                End If
+
+                If strReader IsNot Nothing Then
+                    strReader.Close()
+                    strReader = Nothing
+                End If
+            End Try
+
+            ListFTPFiles = ListOfFilesOnFTPSite
+
+            ListOfFilesOnFTPSite = Nothing
+        End Function
+    End Class
+
+End Namespace
+
+
---
+++ CExp.Core/CExp.Core/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CExp.Core")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("JPO Solutions Limited")> 
+<Assembly: AssemblyProduct("CExp.Core")> 
+<Assembly: AssemblyCopyright("Copyright JPO Solutions Limited©  2014")> 
+<Assembly: AssemblyTrademark("All rights reserved")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("387decb9-14a4-450b-8bbf-95c9a4bce195")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.1")> 
+<Assembly: AssemblyFileVersion("1.0.0.1")> 
